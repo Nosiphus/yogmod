6aa56c9b29b576f2db328897f07e3df7bbee9524
Sean Crain <38871906+seancrain@users.noreply.github.com>
2021-02-17 23:20:58 -0600

    Preliminary work for YogMod 1.16.5 support

---
+++ 1.16.5/src/main/java/com/nosiphus/yogmod/YogMod.java
@@ -0,0 +1,56 @@
+package com.nosiphus.yogmod;
+
+import com.nosiphus.yogmod.util.Reference;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class YogMod {
+
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final ItemGroup YOGTAB = new YogTab("yogtab");
+
+    public YogMod() {
+
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::setup);
+
+        ModItems.ITEMS.register(bus);
+        ModBlocks.BLOCKS.register(bus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    
+
+    }
+
+    public static class YogTab extends ItemGroup {
+
+        public YogTab(String label) {
+
+            super(label);
+
+        }
+
+        @Override
+        public ItemStack createIcon() {
+
+            return ModItems.PENCIL.get().getDefaultInstance();
+
+        }
+
+    }
+
+}
---
+++ 1.16.5/src/main/java/com/nosiphus/yogmod/util/Reference.java
@@ -0,0 +1,15 @@
+package com.nosiphus.yogmod.util;
+
+public class Reference {
+
+    public static final String MODID = "yogmod";
+    public static final String NAME = "YogMod";
+    public static final String VERSION = "1.1.0-2021.02.20";
+    public static final String ACCEPTED_VERSIONS = "[1.16.5]";
+
+    public static final String CLIENT_PROXY_CLASS = "com.nosiphus.yogmod.proxy.ClientProxy";
+    public static final String COMMON_PROXY_CLASS = "com.nosiphus.yogmod.proxy.CommonProxy";
+
+    public static final int GUI_CRATE = 2;
+
+}
