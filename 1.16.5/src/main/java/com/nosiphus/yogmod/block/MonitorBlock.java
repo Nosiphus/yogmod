88a818098989a33797dbce0fb7a3d68ea0f9dc5a
Sean Crain <38871906+seancrain@users.noreply.github.com>
2021-03-11 23:01:44 -0600

    Finally add working rotational monitor

--- 1.16.5/src/main/java/com/nosiphus/yogmod/block/MonitorBlock.java
+++ 1.16.5/src/main/java/com/nosiphus/yogmod/block/MonitorBlock.java
@@ -1,9 +1,9 @@
 package com.nosiphus.yogmod.block;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
-import net.minecraft.block.HorizontalFaceBlock;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
@@ -13,17 +13,15 @@ public class MonitorBlock extends Block {
 
     public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
 
-    public MonitorBlock(Properties builder) {
+    public MonitorBlock(AbstractBlock.Properties properties) {
 
-        super(builder);
+        super(properties);
         this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
 
     }
 
     public BlockState getStateForPlacement(BlockItemUseContext context) {
-
         return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
-
     }
 
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
--- 1.16.5/src/main/java/com/nosiphus/yogmod/init/ModBlocks.java
+++ 1.16.5/src/main/java/com/nosiphus/yogmod/init/ModBlocks.java
@@ -3,13 +3,6 @@ package com.nosiphus.yogmod.init;
 import com.nosiphus.yogmod.block.MonitorBlock;
 import com.nosiphus.yogmod.util.Reference;
 import net.minecraft.block.*;
-import net.minecraft.state.StateContainer;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.shapes.ISelectionContext;
-import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.math.shapes.VoxelShapes;
-import net.minecraft.world.IBlockReader;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -88,12 +81,8 @@ public class ModBlocks {
     public static final RegistryObject<Block> LEVER = BLOCKS.register("lever", () -> new LeverBlock(AbstractBlock.Properties.from(Blocks.LEVER)));
     public static final RegistryObject<Block> LIMESTONE_BRICK = BLOCKS.register("limestone_brick", () -> new Block(AbstractBlock.Properties.from(Blocks.BRICKS)));
     public static final RegistryObject<Block> LINOLEUM_TILE = BLOCKS.register("linoleum_tile", () -> new Block(AbstractBlock.Properties.from(Blocks.STONE)));
-    public static final RegistryObject<Block> LIT_MONITOR = BLOCKS.register("lit_monitor", () -> new MonitorBlock(Block.Properties.from(Blocks.IRON_BLOCK).sound(SoundType.GLASS).setLightLevel((state) -> {
-
-        return 15;
-
-    })));
-    public static final RegistryObject<Block> MONITOR = BLOCKS.register("monitor", () -> new MonitorBlock(Block.Properties.from(Blocks.IRON_BLOCK).sound(SoundType.GLASS)));
+    public static final RegistryObject<Block> LIT_MONITOR = BLOCKS.register("lit_monitor", () -> new MonitorBlock(AbstractBlock.Properties.from(Blocks.JACK_O_LANTERN).sound(SoundType.GLASS)));
+    public static final RegistryObject<Block> MONITOR = BLOCKS.register("monitor", () -> new MonitorBlock(AbstractBlock.Properties.from(Blocks.PUMPKIN).sound(SoundType.GLASS)));
 
     public static final RegistryObject<Block> STEP = BLOCKS.register("step", () -> new Block(AbstractBlock.Properties.from(Blocks.SMOOTH_STONE)));
     public static final RegistryObject<Block> STEP_SLAB = BLOCKS.register("step_slab", () -> new SlabBlock(AbstractBlock.Properties.from(Blocks.SMOOTH_STONE_SLAB)));
