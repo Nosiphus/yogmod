c8a9a0a635bd337f568e337f481ef2bcf64166c7
Sean Crain <38871906+seancrain@users.noreply.github.com>
2022-07-02 20:05:12 -0500

    Add YogMod to 1.18.2

--- src/main/java/com/nosiphus/yogmod/Reference.java
+++
@@ -1,7 +0,0 @@
-package com.nosiphus.yogmod;
-
-public class Reference {
-
-    public static final String MOD_ID = "yogmod";
-
-}
--- src/main/java/com/nosiphus/yogmod/YogMod.java
+++ src/main/java/com/nosiphus/yogmod/YogMod.java
@@ -2,32 +2,30 @@ package com.nosiphus.yogmod;
 
 import com.nosiphus.yogmod.init.ModBlocks;
 import com.nosiphus.yogmod.init.ModItems;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.IForgeRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Objects;
-
-@Mod(Reference.MOD_ID)
+@Mod("yogmod")
 public class YogMod {
 
     public static final Logger LOGGER = LogManager.getLogger();
-    public static final ItemGroup YOGTAB = new YogTab("yogtab");
+
+    public static final CreativeModeTab YOGTAB = new CreativeModeTab("yogmod") {
+        @Override
+        public ItemStack makeIcon() {
+            return ModItems.FLUORESCENT_PANEL.get().getDefaultInstance();
+        }
+    };
 
     public YogMod() {
 
@@ -44,42 +42,24 @@ public class YogMod {
 
     private void clientSetup(final FMLClientSetupEvent event) {
 
-
-        RenderTypeLookup.setRenderLayer(ModBlocks.GLASS.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(ModBlocks.GLASS_PANE.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(ModBlocks.HATCH.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(ModBlocks.IRON_DOOR.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(ModBlocks.LANTERN.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(ModBlocks.WALL_LANTERN.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(ModBlocks.LED.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(ModBlocks.WALL_LED.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(ModBlocks.LADDER.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(ModBlocks.METROVOX_ACTIVATOR_RAIL.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(ModBlocks.METROVOX_DETECTOR_RAIL.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(ModBlocks.METROVOX_RAIL.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(ModBlocks.POWERED_METROVOX_RAIL.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(ModBlocks.WOODEN_DOOR.get(), RenderType.getCutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.GLASS.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.GLASS_PANE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.HATCH.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.IRON_DOOR.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.LANTERN.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.WALL_LANTERN.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.LED.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.WALL_LED.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.LADDER.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.METROVOX_ACTIVATOR_RAIL.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.METROVOX_DETECTOR_RAIL.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.METROVOX_RAIL.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.POWERED_METROVOX_RAIL.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.WOODEN_DOOR.get(), RenderType.cutout());
 
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-
-    }
-
-    public static class YogTab extends ItemGroup {
-
-        public YogTab(String label) {
-
-            super(label);
-
-        }
-
-        @Override
-        public ItemStack createIcon() {
-
-            return new ItemStack(ModBlocks.FLUORESCENT_PANEL.get());
-
-        }
 
     }
 
