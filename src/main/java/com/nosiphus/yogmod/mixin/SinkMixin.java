ea658e73dd20af54b8e4d1d5007ebd6426e26e13
Sean Crain <38871906+seancrain@users.noreply.github.com>
2023-11-02 13:54:56 -0500

    Do some slight code cleanup

--- src/main/java/com/nosiphus/yogmod/mixin/SinkMixin.java
+++ src/main/java/com/nosiphus/yogmod/mixin/SinkMixin.java
@@ -1,12 +1,13 @@
 package com.nosiphus.yogmod.mixin;
 
 import com.nosiphus.yogmod.core.SinkInteraction;
+import net.minecraft.server.Bootstrap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(net.minecraft.server.Bootstrap.class)
+@Mixin(Bootstrap.class)
 public class SinkMixin {
 
     @Inject(method = "bootStrap()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/cauldron/CauldronInteraction;bootStrap()V", shift = At.Shift.AFTER))
