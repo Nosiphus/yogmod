e46828d7e5cda1f8f677dfb7a462bca0b7c0a51e
Sean Crain <38871906+seancrain@users.noreply.github.com>
2023-12-16 21:04:34 -0600

    Update files

---
+++ src/main/java/com/nosiphus/yogmod/world/item/ModTiers.java
@@ -0,0 +1,59 @@
+package com.nosiphus.yogmod.world.item;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum ModTiers implements Tier {
+
+    PENCIL(0, 59, 2.0F, 0.0F, 15, () -> {
+        return Ingredient.of(ItemTags.PLANKS);
+    }),
+    LASER(3, 1561, 8.0F, 3.0F, 10, () -> {
+        return Ingredient.of(Items.DIAMOND);
+    });
+
+    private final int level;
+    private final int uses;
+    private final float speed;
+    private final float damage;
+    private final int enchantmentValue;
+    private final LazyLoadedValue<Ingredient> repairIngredient;
+
+    private ModTiers(int level, int uses, float speed, float damage, int enchantmentValue, Supplier<Ingredient> repairIngredient) {
+        this.level = level;
+        this.uses = uses;
+        this.speed = speed;
+        this.damage = damage;
+        this.enchantmentValue = enchantmentValue;
+        this.repairIngredient = new LazyLoadedValue<>(repairIngredient);
+    }
+
+    public int getUses() {
+        return this.uses;
+    }
+
+    public float getSpeed() {
+        return this.speed;
+    }
+
+    public float getAttackDamageBonus() {
+        return this.damage;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+}
