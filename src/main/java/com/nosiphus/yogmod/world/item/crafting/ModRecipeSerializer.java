538e996ef1edf0316d0e13a2e982aff1e54c8416
Sean <38871906+seancrain@users.noreply.github.com>
2025-02-15 00:39:10 -0600

    Fully implement yogifier recipes

--- src/main/java/com/nosiphus/yogmod/world/item/crafting/ModRecipeSerializer.java
+++ src/main/java/com/nosiphus/yogmod/world/item/crafting/ModRecipeSerializer.java
@@ -10,6 +10,7 @@ public class ModRecipeSerializer {
 
     public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZER = DeferredRegister.create(Registries.RECIPE_SERIALIZER, "yogmod");
 
-    public static final Supplier<RecipeSerializer<YogifierRecipe>> YOGIFIER = RECIPE_SERIALIZER.register("yogifier", YogifierRecipe.Serializer::new);
+    public static final Supplier<RecipeSerializer<YogifierRecipe>> YOGIFIER =
+            RECIPE_SERIALIZER.register("yogifier", YogifierRecipe.Serializer::new);
 
 }
--- src/main/java/com/nosiphus/yogmod/world/item/crafting/ModRecipeType.java
+++ src/main/java/com/nosiphus/yogmod/world/item/crafting/ModRecipeType.java
@@ -1,7 +1,9 @@
 package com.nosiphus.yogmod.world.item.crafting;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -11,6 +13,8 @@ public class ModRecipeType {
 
     public static final DeferredRegister<RecipeType<?>> RECIPE_TYPES = DeferredRegister.create(Registries.RECIPE_TYPE, "yogmod");
 
-    public static final Supplier<RecipeType<YogifierRecipe>> YOGIFIER = RECIPE_TYPES.register("yogifier", () -> RecipeType.<YogifierRecipe>simple(ResourceLocation.fromNamespaceAndPath("yogmod", "yogifier")));
+    public static final Supplier<RecipeType<YogifierRecipe>> YOGIFIER = RECIPE_TYPES.register("yogifier",
+            () -> RecipeType.<YogifierRecipe>simple(ResourceLocation.fromNamespaceAndPath("yogmod", "yogifier"))
+    );
 
 }
