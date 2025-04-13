57eef791d270629260a07bed086dbf8db7149b3d
Sean Crain <38871906+seancrain@users.noreply.github.com>
2025-04-13 17:43:04 -0500

    Rewrite jukebox mixin

--- src/main/java/com/nosiphus/yogmod/mixin/RecordItemMixin.java
+++ src/main/java/com/nosiphus/yogmod/mixin/RecordItemMixin.java
@@ -19,40 +19,23 @@ import net.minecraft.world.level.block.entity.JukeboxBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RecordItem.class)
 public class RecordItemMixin extends Item {
+
     public RecordItemMixin(Properties properties) {
         super(properties);
     }
 
-    /**
-     * @author seancrain
-     * @reason Discs can only be placed in normal jukeboxes. This replaces the method to allow record players to work as well.
-     */
-    @Overwrite
-    public InteractionResult useOn(UseOnContext context) {
+    @Inject(method = "useOn", at = @At("HEAD"), cancellable = true)
+    private void yogmod$useOn(UseOnContext context, CallbackInfoReturnable cir) {
         Level level = context.getLevel();
         BlockPos blockPos = context.getClickedPos();
         BlockState blockState = level.getBlockState(blockPos);
-        if (blockState.is(Blocks.JUKEBOX) && !blockState.getValue(JukeboxBlock.HAS_RECORD)) {
-            ItemStack itemStack = context.getItemInHand();
-            if (!level.isClientSide) {
-                Player player = context.getPlayer();
-                BlockEntity blockEntity = level.getBlockEntity(blockPos);
-                if (blockEntity instanceof JukeboxBlockEntity) {
-                    JukeboxBlockEntity jukeboxBlockEntity = (JukeboxBlockEntity) blockEntity;
-                    jukeboxBlockEntity.setFirstItem(itemStack.copy());
-                    level.gameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Context.of(player, blockState));
-                }
-                itemStack.shrink(1);
-                if (player != null) {
-                    player.awardStat(Stats.PLAY_RECORD);
-                }
-            }
-            return InteractionResult.sidedSuccess(level.isClientSide);
-        } else if (blockState.is(ModBlocks.RECORD_PLAYER.get()) && !blockState.getValue(RecordPlayerBlock.HAS_RECORD)) {
+        if (blockState.is(ModBlocks.RECORD_PLAYER.get()) && !blockState.getValue(RecordPlayerBlock.HAS_RECORD)) {
             ItemStack itemStack = context.getItemInHand();
             if (!level.isClientSide) {
                 Player player = context.getPlayer();
@@ -67,9 +50,7 @@ public class RecordItemMixin extends Item {
                     player.awardStat(Stats.PLAY_RECORD);
                 }
             }
-            return InteractionResult.sidedSuccess(level.isClientSide);
-        } else {
-            return InteractionResult.PASS;
+            cir.setReturnValue(InteractionResult.sidedSuccess(level.isClientSide));
         }
     }
 }
