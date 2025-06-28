8112886ad2c5dd23aa6dbb12a136fa4747929c26
Sean Crain <38871906+seancrain@users.noreply.github.com>
2025-06-28 01:14:30 -0500

    update target date

--- src/main/java/com/nosiphus/yogmod/YogMod.java
+++ src/main/java/com/nosiphus/yogmod/YogMod.java
@@ -10,7 +10,7 @@ import cpw.mods.fml.common.Mod.EventHandler;
 public class YogMod
 {
     public static final String MODID = "yogmod";
-    public static final String VERSION = "2025.06.24";
+    public static final String VERSION = "2025.07.05";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
