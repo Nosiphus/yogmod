818227254135355a2e9834b21332d17f2ff5e1a4
Sean Crain <38871906+seancrain@users.noreply.github.com>
2025-07-04 10:58:16 -0500

    Implement directional support

--- src/main/java/com/nosiphus/yogmod/block/BlockMonitor.java
+++ src/main/java/com/nosiphus/yogmod/block/BlockMonitor.java
@@ -6,7 +6,11 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
 
 public class BlockMonitor extends BlockDirectional {
 
@@ -28,6 +32,13 @@ public class BlockMonitor extends BlockDirectional {
     public IIcon getIcon(int side, int meta)
     {
         return side == 1 ? this.iconTop : (side == 0 ? this.iconTop : (meta == 2 && side == 2 ? this.iconFront : (meta == 3 && side == 5 ? this.iconFront : (meta == 0 && side == 3 ? this.iconFront : (meta == 1 && side == 4 ? this.iconFront : this.blockIcon)))));
+    }
+
+    @Override
+    public void onBlockPlacedBy(World world, int posX, int posY, int posZ, EntityLivingBase entity, ItemStack stack)
+    {
+        int l = MathHelper.floor_double((double)(entity.rotationYaw * 4.0F / 360.0F) + 2.5D) & 3;
+        world.setBlockMetadataWithNotify(posX, posY, posZ, l, 2);
     }
 
     @Override
