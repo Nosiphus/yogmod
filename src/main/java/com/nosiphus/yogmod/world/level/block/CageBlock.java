e71489ba9a3be71f64b1681a494b7004525dc760
Sean Crain <38871906+seancrain@users.noreply.github.com>
2025-08-01 23:44:29 -0500

    Optimize imports

--- src/main/java/com/nosiphus/yogmod/world/level/block/CageBlock.java
+++ src/main/java/com/nosiphus/yogmod/world/level/block/CageBlock.java
@@ -3,21 +3,15 @@ package com.nosiphus.yogmod.world.level.block;
 import com.mojang.serialization.MapCodec;
 import com.nosiphus.yogmod.world.level.block.entity.CageBlockEntity;
 import com.nosiphus.yogmod.world.level.block.entity.ModBlockEntityType;
-import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.*;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.Spawner;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -28,7 +22,6 @@ import net.minecraft.world.level.block.state.BlockState;
 
 import javax.annotation.Nullable;
 import java.util.List;
-import java.util.Optional;
 
 public class CageBlock extends BaseEntityBlock {
 
